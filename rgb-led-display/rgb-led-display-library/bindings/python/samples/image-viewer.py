a9c030126009ebdafd84e4518640dcb714c56aea
beverlybiwenlow <beverlybiwenlow@hotmail.com>
2018-07-11 21:59:25 +0000

    change logo png

--- rgb-led-display/rgb-led-display-library/bindings/python/samples/image-viewer.py
+++ rgb-led-display/rgb-led-display-library/bindings/python/samples/image-viewer.py
@@ -14,8 +14,8 @@ image = Image.open(image_file)
 
 # Configuration for the matrix
 options = RGBMatrixOptions()
-options.rows = 32
-options.chain_length = 1
+options.rows = 16
+options.chain_length = 4
 options.parallel = 1
 options.hardware_mapping = 'regular'  # If you have an Adafruit HAT: 'adafruit-hat'
 
