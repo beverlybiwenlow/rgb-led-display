f9c903764ccc00062e86de9985f4ded7f1ef7f51
beverlybiwenlow <beverlybiwenlow@hotmail.com>
2018-07-11 20:41:00 +0000

    api call fix

--- rgb-led-display/rgb-led-display-library/bindings/python/samples/data-files/getData.py
+++ rgb-led-display/rgb-led-display-library/bindings/python/samples/data-files/getData.py
@@ -10,38 +10,40 @@ url = "http://ec2-54-213-39-245.us-west-2.compute.amazonaws.com:5775"
 endpoint = "/api/v1/totals_allcustomers/"
 
 def success_callback(response):
-    logger.info("API Call Success")
-    data = response.json()
-    f_keys = open('data-keys.txt','w')
-    f_values = open('data-values.txt','w')
-    print(data)
+	logger.info("API Call Success")
+	data = response.json()
+	f_keys = open('data-keys.txt','w')
+	f_values = open('data-values.txt','w')
+	print(data)
 
-    for key in data:
-        key_string = json.dumps(key)
-        key_string = key_string.replace('"', '')
-        key_string = key_string.replace('_', ' ')
-        key_string = key_string.replace('total ', '')
-        f_keys.write(key_string)
-        f_keys.write("\n")
-        
-        if isinstance(data[key], dict):
-            value_list = data[key].values()
-            value_list[0] = "{:,}".format(value_list[0])
-            value_list[1] = value_list[1].replace("gallons", "gal")
-            value_string = value_list[0] + " " + value_list[1]
-        else:
-            num = data[key]
-            value_string = "{:,}".format(num)
-        
-        f_values.write(value_string)
-        f_values.write("\n")
-    
-    f_keys.close()
-    f_values.close()
-    
+	for key in data:
+		key_string = json.dumps(key)
+		key_string = key_string.replace('"', '')
+		key_string = key_string.replace('_', ' ')
+		key_string = key_string.replace('total ', '')
+		f_keys.write(key_string)
+		f_keys.write("\n")
+
+	if isinstance(data[key], dict):
+		value_list = data[key].values()
+		print(value_list)
+		value_list["value"] = "{:,}".format(value_list["value"])
+		value_list["unit"] = value_list["unit"].replace("gallons", "gal")
+		value_string = value_list["value"] + " " + value_list["unit"]
+
+	else:
+		num = data[key]
+		value_string = "{:,}".format(num)
+
+	f_values.write(value_string)
+	f_values.write("\n")
+
+	f_keys.close()
+	f_values.close()
+
 
 def error_callback(response):
-    logger.info("API Call FAIL with status code %i and error %s" %response.status_code %response.error)
+	logger.info("API Call FAIL with status code %i and error %s" %response.status_code %response.error)
 
 
 f = "loadSensorsTotalData.log"
@@ -57,15 +59,13 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 while(True):
-    response = requests.get(url + endpoint)
-    response_status = response.status_code
-    if(response_status == 200):
-        success_callback(response)
-        print("ok")
-        time.sleep(20)
-        #break
-    else:
-        error_callback(response)
-        time.sleep(60)
-
-
+	response = requests.get(url + endpoint)
+	response_status = response.status_code
+	if(response_status == 200):
+		success_callback(response)
+		print("ok")
+		time.sleep(20)
+		#break
+	else:
+		error_callback(response)
+		time.sleep(60)
